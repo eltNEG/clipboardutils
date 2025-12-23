7de75c1929116f6331644e654c9183fd2a509c59
eltneg <yorewole@gmail.com>
2025-12-23 08:02:05 +0000

    Add compact function

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,6 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    // if (args.len > 1) {
     const fields = comptime std.meta.declarations(Operators);
     inline for (fields) |field| {
         if (args.len > 1 and std.mem.eql(u8, field.name, args[1])) {
@@ -42,4 +41,111 @@ const Operators = struct {
         std.debug.print("length: {d}\n", .{data.len});
         return null;
     }
+
+    pub fn reverse(allocator: std.mem.Allocator, data: []const u8) anyerror!?[]u8 {
+        const output = try allocator.alloc(u8, data.len);
+        std.mem.reverse(u8, output);
+        return output;
+    }
+
+    const Quote = enum { Open, Close };
+
+    /// Remove whitespace and newline from the clipboard value
+    pub fn compact(allocator: std.mem.Allocator, data: []const u8) anyerror!?[]u8 {
+        const output = try allocator.alloc(u8, data.len);
+        var j: usize = 0;
+        var quote_state: Quote = .Close;
+        var eat_next = false;
+        for (data) |c| {
+            if (eat_next) {
+                output[j] = c;
+                j += 1;
+                eat_next = false;
+                continue;
+            }
+            switch (c) {
+                '"' => {
+                    quote_state = if (quote_state == .Open) .Close else .Open;
+                    output[j] = c;
+                    j += 1;
+                },
+                '\\' => {
+                    if (quote_state == .Open) {
+                        eat_next = true;
+                    }
+                    output[j] = c;
+                    j += 1;
+                },
+                ' ', '\n' => {
+                    if (quote_state == .Open) {
+                        output[j] = c;
+                        j += 1;
+                    }
+                },
+                else => {
+                    output[j] = c;
+                    j += 1;
+                },
+            }
+        }
+        _ = allocator.resize(output, j);
+
+        return output[0..j];
+    }
 };
+
+test "compact" {
+    const allocator = std.testing.allocator;
+    const input = "Hello, World!";
+    const expected = [_]u8{ 'H', 'e', 'l', 'l', 'o', ',', 'W', 'o', 'r', 'l', 'd', '!' };
+    const actual = try Operators.compact(allocator, input);
+    if (actual) |actual_value| {
+        defer allocator.free(actual_value);
+        try std.testing.expectEqualSlices(u8, &expected, actual_value);
+    }
+}
+
+test "compact with quotes" {
+    const allocator = std.testing.allocator;
+    const input = "\"Hello, World!\".hi johndoe";
+    const expected = [_]u8{ '"', 'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '"', '.', 'h', 'i', 'j', 'o', 'h', 'n', 'd', 'o', 'e' };
+    const actual = try Operators.compact(allocator, input);
+    if (actual) |actual_value| {
+        defer allocator.free(actual_value);
+        try std.testing.expectEqualSlices(u8, &expected, actual_value);
+    }
+}
+
+test "compact with escaped quotes" {
+    const allocator = std.testing.allocator;
+    const input = "\"Hello, World!\".hi johndoe";
+    const expected = [_]u8{ '"', 'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '"', '.', 'h', 'i', 'j', 'o', 'h', 'n', 'd', 'o', 'e' };
+    const actual = try Operators.compact(allocator, input);
+    if (actual) |actual_value| {
+        defer allocator.free(actual_value);
+        try std.testing.expectEqualSlices(u8, &expected, actual_value);
+    }
+}
+
+test "json value" {
+    const jsn =
+        \\[
+        \\  {
+        \\    "name": "John Doe",
+        \\    "age": 30,
+        \\    "email address": "john.doe@example.com",
+        \\    "key": "value\""
+        \\  }
+        \\]
+        \\
+    ;
+    const expected =
+        \\[{"name":"John Doe","age":30,"email address":"john.doe@example.com","key":"value\""}]
+    ;
+    const allocator = std.testing.allocator;
+    const actual = try Operators.compact(allocator, jsn);
+    if (actual) |actual_value| {
+        defer allocator.free(actual_value);
+        try std.testing.expectEqualSlices(u8, expected, actual_value);
+    }
+}
